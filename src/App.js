9358fd90a16de1c221203ce7f421a0c01166fa29
abdul <abdulramiz007@gmail.com>
2022-05-22 14:56:02 +0530

    abdul

--- src/App.js
+++ src/App.js
@@ -1,119 +1,24 @@
 import React, { useState } from "react";
-import Axios from "axios";
-import styled from "styled-components";
-import MovieComponent from "./components/MovieComponent";
-import MovieInfoComponent from "./components/MovieInfoComponent";
+import "./App.css";
 
-export const API_KEY = "38f184bf";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-const AppName = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
-const Header = styled.div`
-  background-color:blue;
-  color: black;
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  align-items: center;
-  padding: 10px;
-  font-size: 25px;
-  font-weight: bold;
-  box-shadow: 0 3px 6px 0 #555;
-`;
-const SearchBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  padding: 10px 10px;
-  border-radius: 6px;
- 
-`;
-const SearchIcon = styled.img`
-  width: 32px;
-  height: 32px;
-`;
-const MovieImage = styled.img`
-  width: 48px;
-  height: 48px;
-  margin: 15px;
-`;
-const SearchInput = styled.input`
-  color: black;
-  font-size: 16px;
-  font-weight: bold;
-
-`;
-const MovieListContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-
-`;
-const Placeholder = styled.img`
-  width: 120px;
-  height: 120px;
-
-`;
-
-function App() {
-  const [searchQuery, updateSearchQuery] = useState("");
-
-  const [movieList, updateMovieList] = useState([]);
-  const [selectedMovie, onMovieSelect] = useState();
-
-  const [timeoutId, updateTimeoutId] = useState();
-
-  const fetchData = async (searchString) => {
-    const response = await Axios.get(
-      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`,
-    );
-    updateMovieList(response.data.Search);
-  };
-
-  const onTextChange = (e) => {
-    onMovieSelect("")
-    clearTimeout(timeoutId);
-    updateSearchQuery(e.target.value);
-    const timeout = setTimeout(() => fetchData(e.target.value), 500);
-    updateTimeoutId(timeout);
-  };
+//☀︎ ☽
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <Container>
-      <Header>
-        <AppName>
-          <MovieImage src="/react-movie-app/movie-icon.svg" />
-          React Movie App
-        </AppName>
-        <SearchBox>
-          <SearchIcon src="/react-movie-app/search-icon.svg" />
-          <SearchInput
-            placeholder="Search Movie"
-            value={searchQuery}
-            onChange={onTextChange}
-          />
-        </SearchBox>
-      </Header>
-      {selectedMovie && <MovieInfoComponent selectedMovie={selectedMovie} onMovieSelect={onMovieSelect}/>}
-      <MovieListContainer>
-        {movieList?.length ? (
-          movieList.map((movie, index) => (
-            <MovieComponent
-              key={index}
-              movie={movie}
-              onMovieSelect={onMovieSelect}
-            />
-          ))
-        ) : (
-          <Placeholder src="/react-movie-app/movie-icon.svg" />
-        )}
-      </MovieListContainer>
-    </Container>
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      <div className="container">
+        <span style={{ color: darkMode ? "grey" : "yellow" }}>☀︎</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? "#c96dfd" : "grey" }}>☽</span>
+      </div>
+      <div>
+        <h1>Cool its {darkMode ? "Dark" : "Light"} Mode </h1>
+      </div>
+    </div>
   );
 }
-
-export default App;
